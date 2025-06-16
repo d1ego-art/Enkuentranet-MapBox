31bef0345724a13ad12fdcb11f7632eb53f90ffa
d1ego-art <145490502+d1ego-art@users.noreply.github.com>
2025-06-16 14:35:14 -0400

    Geolocation

--- src/assets/components/EncontrarSection.jsx
+++ src/assets/components/EncontrarSection.jsx
@@ -5,8 +5,7 @@ const EncontrarSection = () => {
   return (
     <section>
       <h2>Encuentra tu Lugar</h2>
-      <MapboxMap latitude={-33.438} longitude={-70.651} zoom={9} 
-      Pitch={20}/>
+      <MapboxMap latitude={-33.438} longitude={-70.651} zoom={10}/>
       {/* Otros contenidos */}
     </section>
   );
--- src/assets/components/MapBoxMap.jsx
+++ src/assets/components/MapBoxMap.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import Map, { Marker } from 'react-map-gl/mapbox'; //archivo de node_modules
+import React, { useState } from 'react';
+import Map, { GeolocateControl } from 'react-map-gl/mapbox'; //archivo de node_modules
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiZDFlZ28tYXJ0IiwiYSI6ImNtYnU0ZDJtdzAwZHEybG45OWo1cTIyZzEifQ.8Y_DuVk8DEoD-iNp17_klQ'; //token mapa personal 
-const mapStyle = "mapbox://styles/d1ego-art/cmbv4rj3l019d01s6ckfj601j"; // Puedes cambiar el estilo
+const mapStyle = "mapbox://styles/d1ego-art/cmbzabkwd01hs01s20pqp566c"; // Puedes cambiar el estilo
 
-const MapboxMap = ({ latitude = -33.449, longitude = -70.671, zoom = 12 , pitch = 20}) => {
+const MapboxMap = () => { 
+ 
   return (
-    <div style={{ width: '100%', height: '600px', borderRadius: '10px', overflow: 'hidden', boxShadow: '0 2px 8px #aaa' }}>
+    <div style={{ 
+      width: '100%',
+       height: '600px',
+        borderRadius: '10px',
+         overflow: 'hidden',
+          boxShadow: '0 8px 8px #aaa'
+           }}>
       <Map
-        initialViewState={{
-          longitude,
-          latitude,
-          zoom,
-          pitch
+         initialViewState={{
+          longitude: -70.651,
+          latitude: -33.438,
+          zoom: 10,
+          pitch: 60
         }}
         style={{ width: '100%', height: '100%' }}
         mapStyle={mapStyle}
         mapboxAccessToken={MAPBOX_TOKEN}
-      >
-        <Marker longitude={longitude} latitude={latitude} color="green
-        " />
+        >
+        <GeolocateControl
+        position='top-left' //ubicación geoicon
+        positionOptions={{enableHighAccuracy: true}}
+         trackUserLocation={true}
+        showUserHeading={true}
+        />
+
+
       </Map>
     </div>
   );
